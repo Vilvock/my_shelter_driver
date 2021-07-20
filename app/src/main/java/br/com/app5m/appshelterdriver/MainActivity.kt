f52a5122025415d4413c990bad5ca70daab9ac68
joao <joaocapitaneo@gmail.com>
2021-07-20 14:08:58 -0300

    Initial commit

---
+++ app/src/main/java/br/com/app5m/appshelterdriver/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.app5m.appshelterdriver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
