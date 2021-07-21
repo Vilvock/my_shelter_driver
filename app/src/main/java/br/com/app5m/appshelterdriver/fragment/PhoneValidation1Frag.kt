959bc60608e173e64b9bf5f8cff2ec7a7873cd48
joao <joaocapitaneo@gmail.com>
2021-07-21 17:17:50 -0300

    ta tudo uma bosta

--- app/src/main/java/br/com/app5m/appshelterdriver/fragment/PhoneValidation1Frag.kt
+++ app/src/main/java/br/com/app5m/appshelterdriver/fragment/PhoneValidation1Frag.kt
@@ -6,19 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import br.com.app5m.appshelterdriver.R
-import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.fragment_phonenumber_validation1.*
 import kotlinx.android.synthetic.main.fragment_phonenumber_validation1.view.*
 
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PhonenumberValidation1Fragment.newInstance] factory method to
+ * Use the [PhoneValidation1Frag.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PhonenumberValidation1Fragment : Fragment() {
+class PhoneValidation1Frag : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,15 +29,15 @@ class PhonenumberValidation1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_phonenumber_validation1, container, false)
+        return inflater.inflate(R.layout.fragment_phone_validation1, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val items = listOf("93", "55", "61", "257")
-        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
+        val adapter = ArrayAdapter(requireContext(), R.layout.adapter_list_codcountry, items)
 //        phoneCountryTi.phoneCountryAcTv?.setAdapter(adapter)
-        phoneCountryAcTv.setText(arrayAdater.getItem)
+        phoneCountryAcTv.setText(adapter.getItem(0).toString(),false)
 
     }
 }
