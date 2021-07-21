564c2f42c3bfbcfaeb659467c519b5324eece9e3
joao <joaocapitaneo@gmail.com>
2021-07-21 17:24:21 -0300

    ta tudo uma bosta

--- app/src/main/java/br/com/app5m/appshelterdriver/fragment/PhoneValidation1Frag.kt
+++ app/src/main/java/br/com/app5m/appshelterdriver/fragment/PhoneValidation1Frag.kt
@@ -7,8 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import br.com.app5m.appshelterdriver.R
-import kotlinx.android.synthetic.main.fragment_phonenumber_validation1.*
-import kotlinx.android.synthetic.main.fragment_phonenumber_validation1.view.*
+import kotlinx.android.synthetic.main.fragment_phone_validation1.*
+
 
 
 /**
