200cf9e95173faa960a9cbb4843a53583155f50f
joao <joaocapitaneo@gmail.com>
2021-07-22 01:27:30 -0300

    vamo ve

--- app/src/main/java/br/com/app5m/appshelterdriver/fragment/auth/phoneValidation/PhoneValidation1Frag.kt
+++ app/src/main/java/br/com/app5m/appshelterdriver/fragment/auth/phoneValidation/PhoneValidation1Frag.kt
@@ -1,4 +1,4 @@
-package br.com.app5m.appshelterdriver.fragment
+package br.com.app5m.appshelterdriver.fragment.auth.phoneValidation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
---
+++ app/src/main/java/br/com/app5m/appshelterdriver/fragment/auth/phoneValidation/PhoneValidation2Frag.kt
@@ -0,0 +1,43 @@
+package br.com.app5m.appshelterdriver.fragment.auth.phoneValidation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import br.com.app5m.appshelterdriver.R
+import kotlinx.android.synthetic.main.fragment_phone_validation1.*
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [PhoneValidation2Frag.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class PhoneValidation2Frag : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_phone_validation2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val items = listOf("93", "55", "61", "257")
+        val adapter = ArrayAdapter(requireContext(), R.layout.adapter_list_codcountry, items)
+//        phoneCountryTi.phoneCountryAcTv?.setAdapter(adapter)
+        phoneCountryAcTv.setText(adapter.getItem(0).toString(),false)
+
+    }
+}
