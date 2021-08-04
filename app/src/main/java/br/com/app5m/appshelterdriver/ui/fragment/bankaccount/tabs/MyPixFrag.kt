0c77b6b6212061a0f652a5ab296a69868c80eb29
joao <joaocapitaneo@gmail.com>
2021-08-04 14:52:51 -0300

    balalaica

--- app/src/main/java/br/com/app5m/appshelterdriver/ui/fragment/bankaccount/tabs/MyPixFrag.kt
+++ app/src/main/java/br/com/app5m/appshelterdriver/ui/fragment/bankaccount/tabs/MyPixFrag.kt
@@ -6,14 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.com.app5m.appshelterdriver.R
+import br.com.app5m.appshelterdriver.helper.RecyclerItemClickListener
+import br.com.app5m.appshelterdriver.model.Travel
+import br.com.app5m.appshelterdriver.ui.adapter.MyBankAdapter
+import br.com.app5m.appshelterdriver.ui.adapter.MyPixAdapter
 import br.com.app5m.appshelterdriver.ui.fragment.bankaccount.AddPixFrag
 import br.com.app5m.appshelterdriver.util.Useful
+import kotlinx.android.synthetic.main.fragment_my_bank.*
 import kotlinx.android.synthetic.main.fragment_my_pix.*
 
 
-class MyPixFrag : Fragment() {
+class MyPixFrag : Fragment(), RecyclerItemClickListener {
     private lateinit var useful: Useful
+    private var listTravelHistorys  = ArrayList<Travel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +45,33 @@ class MyPixFrag : Fragment() {
             activity?.let { it1 -> useful.startFragmentOnBack(AddPixFrag(), it1.supportFragmentManager) }
 
         }
+        configureInitialViews()
 
+        listTravelHistorys.add(Travel())
+        listTravelHistorys.add(Travel())
+        listTravelHistorys.add(Travel())
+        listTravelHistorys.add(Travel())
     }
 
+    fun configureInitialViews() {
+        my_pix_rv.apply {
+            setHasFixedSize(true)
+            val itemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+            itemDecoration.setDrawable(
+                resources.getDrawable(
+                    R.drawable.item_decoration_layout_no_bg,
+                    null
+                )
+            )
+            my_pix_rv.addItemDecoration(itemDecoration)
+
+        }
+        val myAdressesAdapter = MyPixAdapter(requireContext(), listTravelHistorys, this)
+
+        val layoutManagerAdresses: RecyclerView.LayoutManager = GridLayoutManager(context, 1)
+
+        my_pix_rv.layoutManager = layoutManagerAdresses
+
+        my_pix_rv.adapter = myAdressesAdapter
+    }
 }
