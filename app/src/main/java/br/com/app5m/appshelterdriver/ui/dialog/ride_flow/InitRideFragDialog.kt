25d5e8a1c64dbfb34f2bdf6653b19b4a41af6b04
Vilvock <64989193+Vilvock@users.noreply.github.com>
2021-11-09 18:09:00 -0300

    safer

--- app/src/main/java/br/com/app5m/appshelterdriver/ui/dialog/ride_flow/InitRideFragDialog.kt
+++ app/src/main/java/br/com/app5m/appshelterdriver/ui/dialog/ride_flow/InitRideFragDialog.kt
@@ -1,16 +1,23 @@
 package br.com.app5m.appshelterdriver.ui.dialog.ride_flow
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import br.com.app5m.appshelterdriver.R
+import br.com.app5m.appshelterdriver.controller.RideControl
 import br.com.app5m.appshelterdriver.controller.webservice.WSResult
+import br.com.app5m.appshelterdriver.models.Ride
+import br.com.app5m.appshelterdriver.ui.activity.HomeAct
+import br.com.app5m.appshelterdriver.util.Preferences
+import br.com.app5m.appshelterdriver.util.Useful
+import br.com.app5m.appshelterdriver.util.visual.SingleToast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.default_bottom_sheet_dialog_container.*
-import kotlinx.android.synthetic.main.loading.*
+import kotlinx.android.synthetic.main.dialog_bottom_view_acceptride.*
+import kotlinx.android.synthetic.main.dialog_bottom_view_initride.*
 
 
 /**
@@ -19,13 +26,12 @@ import kotlinx.android.synthetic.main.loading.*
 class InitRideFragDialog (private val bottomSheetDialogFragment: BottomSheetDialogFragment)
     : BottomSheetDialogFragment(), WSResult {
 
-//    private lateinit var useful: Useful
-//    private lateinit var rideControl: RideControl
-//    private lateinit var mapControl: MapControl
-//
-//    private lateinit var preferences: Preferences
-//
-//    private lateinit var homeActContext: HomeAct
+    private lateinit var useful: Useful
+    private lateinit var rideControl: RideControl
+
+    private lateinit var preferences: Preferences
+
+    private lateinit var homeActContext: HomeAct
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,128 +43,43 @@ class InitRideFragDialog (private val bottomSheetDialogFragment: BottomSheetDial
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        
+        useful = Useful(requireContext())
+        preferences = Preferences(requireContext())
+        rideControl = RideControl(requireContext(), this, useful)
 
-        bottomSheetDialogFragment.close_imageButton.visibility = View.GONE
-//        useful = Useful(requireContext())
-//        rideControl = RideControl(requireContext(), this, useful)
-//        mapControl = MapControl(requireContext(), this, useful)
-//
-//        homeActContext = requireActivity() as HomeAct
-//
-//
-//        if (homeActContext.rideLiveData.value!!.rideStatus == null) {
-//            val mapRoutes = Map()
-//
-//            mapRoutes.originLatitude = homeActContext.rideLiveData.value!!.originLatitude
-//            mapRoutes.originLongitude = homeActContext.rideLiveData.value!!.originLongitude
-//            mapRoutes.destinationLatitude = homeActContext.rideLiveData.value!!.destinationLatitude
-//            mapRoutes.destinationLongitude = homeActContext.rideLiveData.value!!.destinationLongitude
-//
-//            mapControl.searchRouteByCoordinate(mapRoutes)
-//        }
-//
-//        loading.setOnClickListener {
-//
-//            homeActContext.notifyScreenStageChanged(HomeAct.MainScreenStage.WAITING_PICKUP)
-//            bottomSheetDialogFragment.dismiss()
-//
-//        }
-//
-//        swipe_bt.setOnStateChangeListener {
-//
-//            useful.showDefaultDialogView(requireActivity().supportFragmentManager, "cancel")
-//            bottomSheetDialogFragment.dismiss()
-//        }
-//
-//
-//    }
-//
-//    override fun mResponse(response: Map, type: String) {
-//
-//        val routesDetails = response
-//
-//        if (type == "searchByCoordinate") {
-//            if (routesDetails.status == "OK") {
-//
-//                //               "id_de": 11,
-//                //               "endereco_in": "Av. Pres. Getúlio Vargas 5019 Alvorada",
-//                //               "endereco_out": "Av. Tiradentes 81 Alvorada",
-//                //               "latitude_in": "-30.003922",
-//                //               "longitude_in": "-51.052373",
-//                //               "latitude_out": "-30.0137134",
-//                //               "longitude_out": "-51.0847039",
-//                //               "distancia": "4",
-//                //               "distancia_sigla": "km",
-//                //               "tempo_rota": "10",
-//                //               "tipo_carro": 1,
-//                //               "forma_pagamento": 2
-//
-//                val requestRide = Ride()
-//
-//                requestRide.originAddress = homeActContext.rideLiveData.value!!.origin
-//                requestRide.destinationAddress = homeActContext.rideLiveData.value!!.destination
-//                requestRide.originLatitude = homeActContext.rideLiveData.value!!.originLatitude
-//                requestRide.originLongitude = homeActContext.rideLiveData.value!!.originLongitude
-//                requestRide.destinationLatitude = homeActContext.rideLiveData.value!!.destinationLatitude
-//                requestRide.destinationLongitude = homeActContext.rideLiveData.value!!.destinationLongitude
-//                requestRide.distance = routesDetails.routeList[0].legList[0].distance.text!!.replace(" km", "")
-//                requestRide.distanceInitials = "km"
-//                requestRide.routeTime = routesDetails.routeList[0].legList[0].duration.text!!.replace(" mins", "")
-//                requestRide.carType = homeActContext.rideLiveData.value!!.carType
-//                requestRide.paymentMethod = homeActContext.rideLiveData.value!!.paymentMethod
-//
-//                //            "card_id": "CRC-S4MZWXFWEGXR",
-//                //            "cvv": "123",
-//                //            "id_moip": "CUS-OA5QFB7AZT4T"
-//                if (homeActContext.rideLiveData.value!!.paymentMethod == "1") {
-//                    requestRide.cardId = homeActContext.rideLiveData.value!!.cardId
-//                    requestRide.cardId = homeActContext.rideLiveData.value!!.cvv
-//                    requestRide.cardId = preferences.getUserData()!!.moipId
-//                }
-//
-//                Log.d("TAG", "request ride" + Gson().toJson(requestRide))
-//
-//                rideControl.requestRide(requestRide)
-//            } else {
-//
-//                SingleToast.INSTANCE.show(requireContext(), "Não foi possível traçar uma rota para sua viagem, tente novamente mais tarde.", Toast.LENGTH_LONG)
-//                bottomSheetDialogFragment.dismiss()
-//            }
-//        }
-//
-//
-//    }
+        val homeActContext = requireActivity() as HomeAct
 
-//    override fun rResponse(list: List<Ride>, type: String) {
-//
-//        val solicitedRideInfo = list[0]
-//
-//        homeActContext._rideLiveData.value = solicitedRideInfo
-//
-////        [
-////            {
-////                "status":"01",
-////                "msg":"Corrida solicitada.",
-////                "id_corrida":105,
-////                "id_de":"11",
-////                "endereco_in":"Alvorada - RS, Brasil",
-////                "endereco_out":"Porto Alegre, RS, Brasil",
-////                "latitude_in":"-29.999906655234",
-////                "longitude_in":"-51.0784874483943",
-////                "latitude_out":"-30.0368176",
-////                "longitude_out":"-51.2089887",
-////                "distancia":"23.6 km",
-////                "distancia_sigla":"km",
-////                "tempo_rota":null,
-////                "polyline":null,
-////                "valor_total":" R$ 5,50",
-////                "status_corrida":1,
-////                "forma_pagamento":"Dinheiro",
-////                "tipo_carro":"1"
-////            }
-////        ]
-//
+        init_bt.setOnClickListener {
+
+            val initRide = Ride()
+
+            //diogo precisa me trazer vehicleboard e vehicle model no login
+            initRide.id = homeActContext.rideLiveData.value!!.rideId
+
+            Log.d("TAG", "init:")
+
+            rideControl.acceptRide(initRide)
+        }
+
+
     }
 
+    override fun rResponse(list: List<Ride>, type: String) {
 
+        val rideInfo = list[0]
+
+        if (rideInfo.status == "01") {
+            homeActContext.isCameraLock = true
+            homeActContext.notifyScreenStageChanged(HomeAct.MainScreenStage.WAITING_PICKUP)
+
+        }
+
+        SingleToast.INSTANCE.show(requireContext(), rideInfo.msg!!, Toast.LENGTH_LONG)
+        bottomSheetDialogFragment.dismiss()
+
+//        [{"status":"01","msg":"Corrida aceita \n dirija-se at\u00e9 o ponto de partida para dar in\u00edcio \u00e0 corrida."}]
+//
+//
+    }
 }
