3861f5d5fa7a5a1fc78caea7220385b2a9a1480a
Vilvock <64989193+Vilvock@users.noreply.github.com>
2022-01-14 15:26:45 -0300

    sexo

--- app/src/main/java/br/com/app5m/appshelterdriver/config/RetrofitInitializer.kt
+++ app/src/main/java/br/com/app5m/appshelterdriver/config/RetrofitInitializer.kt
@@ -1,11 +1,21 @@
 package br.com.app5m.appshelterdriver.config
 
+import android.os.Build
 import br.com.app5m.appshelterdriver.controller.webservice.WSConstants
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 
 /**
@@ -14,19 +24,64 @@ import java.util.concurrent.TimeUnit
  */
 class RetrofitInitializer {
 
-    fun retrofit(log_enable: Boolean): Retrofit {
+    fun retrofit(log_enable: Boolean): Retrofit{
+
         val logging = HttpLoggingInterceptor()
         logging.level = HttpLoggingInterceptor.Level.BODY
 
-        val client = if (log_enable) OkHttpClient.Builder().connectTimeout(25, TimeUnit.SECONDS)
-            .writeTimeout(25, TimeUnit.SECONDS).readTimeout(25, TimeUnit.SECONDS)
-            .addInterceptor(logging).build()
+        val client = if (log_enable) getOkHttpBuilder().connectTimeout(25, TimeUnit.SECONDS)
+            .writeTimeout(25, TimeUnit.SECONDS).readTimeout(25, TimeUnit.SECONDS).addInterceptor(logging).build()
         else
-            OkHttpClient.Builder().connectTimeout(25, TimeUnit.SECONDS)
+            getOkHttpBuilder().connectTimeout(25, TimeUnit.SECONDS)
                 .writeTimeout(25, TimeUnit.SECONDS).readTimeout(25, TimeUnit.SECONDS).build()
 
-        return Retrofit.Builder().baseUrl(WSConstants.WS_URL_BASE)
-            .addConverterFactory(GsonConverterFactory.create()).client(client).build()
+        val gson: Gson = GsonBuilder().setLenient().create()
+
+        return Retrofit.Builder()
+            .baseUrl(WSConstants.WS_URL_BASE)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
     }
+
+    private fun getOkHttpBuilder(): OkHttpClient.Builder =
+        if (Build.VERSION.SDK_INT in 23..25) {
+            getUnsafeOkHttpClient()
+        } else {
+            // Workaround for the error "Caused by: com.android.org.bouncycastle.jce.exception.ExtCertPathValidatorException:
+                // Could not validate certificate: Certificate expired at".
+            OkHttpClient.Builder()
+    }
+
+    private fun getUnsafeOkHttpClient(): OkHttpClient.Builder =
+        try {
+            // Create a trust manager that does not validate certificate chains
+            val trustAllCerts: Array<TrustManager> = arrayOf(
+                object : X509TrustManager {
+                    @Throws(CertificateException::class)
+                    override fun checkClientTrusted(chain: Array<X509Certificate?>?,
+                                                    authType: String?) = Unit
+
+                    @Throws(CertificateException::class)
+                    override fun checkServerTrusted(chain: Array<X509Certificate?>?,
+                                                    authType: String?) = Unit
+
+                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+                }
+            )
+            // Install the all-trusting trust manager
+            val sslContext: SSLContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
+            val builder = OkHttpClient.Builder()
+            builder.sslSocketFactory(sslSocketFactory,
+                trustAllCerts[0] as X509TrustManager
+            )
+            builder.hostnameVerifier { _, _ -> true }
+            builder
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
 
 }
