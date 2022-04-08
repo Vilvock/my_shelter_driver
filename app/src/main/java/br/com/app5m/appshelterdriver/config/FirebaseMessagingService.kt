aeacf5ab19a9cbaeb2296ec1d8d610615b0eb6d7
Vilvock <64989193+Vilvock@users.noreply.github.com>
2022-04-08 11:32:24 -0300

    safer

--- app/src/main/java/br/com/app5m/appshelterdriver/config/FirebaseMessagingService.kt
+++ app/src/main/java/br/com/app5m/appshelterdriver/config/FirebaseMessagingService.kt
@@ -5,7 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
-import android.media.RingtoneManager
+import android.media.MediaPlayer
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -15,10 +15,13 @@ import br.com.app5m.appshelterdriver.ui.activity.HomeAct
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+
 class FirebaseMessagingService: FirebaseMessagingService() {
 
     private val TAG = "notifica"
 //    private var intent: Intent? = null
+
+    private var mp: MediaPlayer? = null
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
@@ -46,7 +49,7 @@ class FirebaseMessagingService: FirebaseMessagingService() {
         var REQUEST_CODE = 0 //DEFAULT
 
         val channel = getString(R.string.default_notification_channel_id)
-        val uriSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+//        val uriSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
         var notifyScreenValue: HomeAct.MainScreenStage? = null
 
@@ -69,6 +72,7 @@ class FirebaseMessagingService: FirebaseMessagingService() {
 
                 Log.d("tag", "recebido: " + rideId)
 
+                executeSound()
             }
             "2", //ACEITA
             "3", //EM ANDAMENTO
@@ -107,7 +111,7 @@ class FirebaseMessagingService: FirebaseMessagingService() {
             .setContentTitle(title)
             .setContentText(body)
             .setSmallIcon(R.mipmap.ic_launcher)
-            .setSound(uriSound)
+//            .setSound(uriSound)
             .setAutoCancel(true)
             .setContentIntent(pendingIntent)
         val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
@@ -120,7 +124,18 @@ class FirebaseMessagingService: FirebaseMessagingService() {
         }
         notificationManager.notify(0, notification.build())
 
+
     }
 
+    private fun executeSound() {
+
+        if (mp != null) {
+            mp!!.stop()
+            mp!!.release()
+        }
+
+        mp = MediaPlayer.create(applicationContext, R.raw.notification_sound)
+        mp!!.start()
+    }
 
 }
